1feffae62c1e50793d2bcf7217bf852280af37b3
Sonu Kumar Gautam <sonukumargautam41638@gmail.com>
2021-05-25 21:14:32 +0530

    class and functional component

---
+++ src/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+class App extends React.Component {
+  render() {
+    return (<h1>Class Base component</h1>)
+  }
+}
+
+
+/*function App() {
+  return (<h1>This is functional component!</h1>);
+};
+*/
+export default App;
