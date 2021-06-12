21593d7e67f55ccc7a87e8333fb351a9b341cd4f
Sonu Kumar Gautam <sonukumargautam41638@gmail.com>
2021-06-12 22:51:53 +0530

    react hooks

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css';
 import Header from '../Header/Header';
 import SearchBox from '../SearchBox/SearchBox';
@@ -7,7 +7,31 @@ import ResultsContainer from '../ResultsContainer/ResultsContainer';
 
 const name = require('@rstacruz/startup-name-generator');
 
-class App extends React.Component {
+
+const App = () => {
+
+  const [headerText, setHeaderText] = useState('Name It!');
+  const [headerExpanded, setHeaderExpanded] = useState(true);
+  const [suggestedNames, setSuggestedNames] = useState([]);
+
+  const handleInputChange = (inputText) => {
+    setHeaderExpanded(!inputText);
+    setSuggestedNames(inputText ? name(inputText) : []);
+  }
+
+
+  return (
+    <div>
+      <Header headerExpand={headerExpanded} headTitle={headerText} />
+      <SearchBox onInputChange={handleInputChange} />
+      <ResultsContainer suggestedNames={suggestedNames} />
+
+    </div>
+  );
+
+}
+
+/*class App extends React.Component {
 
   state = {
     headerText: "Name It!",
@@ -38,7 +62,7 @@ class App extends React.Component {
 }
 
 
-/*function App() {
+function App() {
   return (<h1>This is functional component!</h1>);
 };
 */
